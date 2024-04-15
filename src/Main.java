e65e9913f74aea9808f0b4ee059b796eca25f3cd
Aiym <aiympernebay@MacBook-Pro-Aiym.local>
2024-04-15 09:47:33 +0500

    defence task

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your string:");
+        String lowerCaseString = scanner.nextLine();
+        String upperCaseString = convertToUpperCase(lowerCaseString);
+        System.out.println("Uppercase string: " + upperCaseString);
+    }
+
+    public static String convertToUpperCase(String str) {
+        if (str.isEmpty()) {
+            return str;
+        } else {
+
+            char firstChar = Character.toUpperCase(str.charAt(0));
+            String restOfString = convertToUpperCase(str.substring(1));
+            return firstChar + restOfString;
+        }
+    }
+}
+
